95e203bc211a8324950168cdfd6ec96bf3b51996
Ayaz Maroof <maroof.ayaz@gmail.com>
2023-08-03 12:24:51 -0500

    Add navbar and quiz data.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import quizData from '../quiz-data.json';
+
+const questions = quizData;
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ol>
+      <li v-for="question in questions" v-bind:key="question.id">
+        <span>{{ question.questionText }}</span>
+        <div>
+          <span v-for="choice in question.choices" :key="choice.id">
+            <span v-if="question.type === 'singleChoice'">
+              <input type="radio" :name="'question'.concat(String(question.id))" :id="choice.id">
+              <label :for="choice.id">{{ choice.text }}</label>
+            </span>
+            <span v-if="question.type === 'multipleChoice'">
+              <input type="checkbox" :id="choice.id">
+              <label :for="choice.id">{{ choice.text }}</label>
+            </span>
+            <br />
+
+          </span>
+        </div>
+      </li>
+    </ol>
   </main>
 </template>
